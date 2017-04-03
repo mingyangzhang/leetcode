afbb64cf59f28d4c9509ff079106469173932c4c
MingYang Zhang <MingYang Zhang>
2017-04-02 21:14:30 -0700

    DP oj72

--- src/DP/OJ72.java
+++ src/DP/OJ72.java
@@ -4,32 +4,33 @@ package DP;
  * Created by mingyazh on 4/1/17.
  */
 public class OJ72 {
-    public int minDistance(String word1, String word2, int shift1, int shift2) {
-        String s1 = word1.substring(shift1);
-        String s2 = word2.substring(shift2);
-        int m = s1.length();
-        int n = s2.length();
+    String s1;
+    String s2;
+    int[][] distance;
+    public int minDistance(String word1, String word2) {
+        this.s1 =  word1;
+        this.s2 = word2;
+        int m = word1.length();
+        int n = word2.length();
         if(m==0 || n==0) return Math.abs(m-n);
-        if(s1.charAt(0) == s2.charAt(0)) return minDistance(word1, word2, shift1+1, shift2+1);
-        else return 1 + minDistance(word1, word2, shift1+1, shift2+1);
-    }
-
-    public int minDistance(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-        if(m==0 || n==0) return Math.abs(m-n);
-        if(m<n) return minDistance(s2, s1);
-        int k = minDistance(s1, s2, 0, 0);
-        for(int i=1; i<k; i++){
-            int p = i+minDistance(s1, s2, i, 0);
-            if(p<k) k=p;
+        distance = new int[m+1][n+1];
+        for(int i=0; i<=m; i++)
+            distance[i][0] = i;
+        for(int i=1; i<=n; i++)
+            distance[0][i] = i;
+        for(int i=1; i<=m; i++)
+            for(int j=1; j<=n; j++){
+            if(s1.charAt(i-1) == s2.charAt(j-1)) distance[i][j] = distance[i-1][j-1];
+            else{
+                distance[i][j] = 1 + Math.min(Math.min(distance[i-1][j], distance[i][j-1]), distance[i-1][j-1]);
+            }
         }
-        return k;
+        return distance[m][n];
     }
 
     public static void main(String[] args){
-        String s1 = "abc";
-        String s2 = "aabc";
+        String s1 = "zoologicoarchaeologist";
+        String s2 = "zoogeologist";
         OJ72 sol = new OJ72();
         System.out.print(sol.minDistance(s1, s2));
     }
