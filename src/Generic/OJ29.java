a8721d746215e7c221af1bb3cd953cc3d5306a0a
mingyangzhang <zmyrzwd@gmail.com>
2017-04-26 21:46:43 -0700

    OJ29

---
+++ src/Generic/OJ29.java
@@ -0,0 +1,22 @@
+public class OJ29 {
+    public int divide(int dividend, int divisor) {
+        if(dividend == 0) return 0;
+        if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+        boolean isNegative = (dividend < 0) != (divisor < 0);
+        long longDividend = Math.abs(Long.valueOf(dividend));
+        long longDivisor = Math.abs(Long.valueOf(divisor));
+        int ans = 0;
+        while(longDividend >= longDivisor){
+            int i = 1;
+            long temp = longDivisor;
+            while(longDividend>=temp){
+                longDividend -= temp;
+                ans += i;
+                temp <<= 1;
+                i <<= 1;
+            }
+        }
+        if(ans>Integer.MAX_VALUE) return isNegative?Integer.MIN_VALUE:Integer.MAX_VALUE;
+        return isNegative?-ans:ans;
+    }
+}
